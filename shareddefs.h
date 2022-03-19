5307b4aea9de6c0057fc19c30dd47e9c41f7bb4f
AhmedSalihCezayir <asalihcezayir@gmail.com>
2022-03-19 13:46:20 +0300

    Struct defs

---
+++ shareddefs.h
@@ -0,0 +1,24 @@
+#include <pthread.h>
+
+struct pcb {
+    int pid;
+    pthread_t tid;
+    int state; // 1- READY, 2- RUNNING, 3-WAITING
+    long long next_cpuburst_len;
+    long long rem_cpuburt_len;
+    long long num_cpuburst;
+    long long time_spend_ready;
+    int device1_io_count;
+    int device2_io_count;
+    long long arrival_time;
+    long long finish_time;
+    long long total_exec_time;
+};
+
+struct cpu {
+    struct prc;
+};
+
+struct io {
+    struct prc;
+};
