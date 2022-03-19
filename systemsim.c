aabe464ba7b5b81a8f68121b6b1133ecad3e3fb0
AhmedSalihCezayir <asalihcezayir@gmail.com>
2022-03-19 14:18:09 +0300

    arguments taken

---
+++ systemsim.c
@@ -0,0 +1,30 @@
+#include "shareddefs.h"
+#include "queue.c"
+#include <pthread.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    char* ALG = argv[1];
+    long long Q = strtoll(argv[2], NULL, 10);
+    int T1 = atoi(argv[3]);
+    int T2 = atoi(argv[4]);
+    char* burst_dist = argv[5];
+    long long burstlen = strtoll(argv[6], NULL, 10);
+    long long min_burst = strtoll(argv[7], NULL, 10);
+    long long max_burst = strtoll(argv[8], NULL, 10);
+    double p0 = strtod(argv[9], NULL);
+    double p1 = strtod(argv[10], NULL);
+    double p2 = strtod(argv[11], NULL);
+    double pg = strtod(argv[12], NULL);
+    int MAXP = atio(argv[13]);
+    int ALLP = atoi(argv[14]);
+    int OUTMODE = atoi(argv[15]);
+
+    printf("%s, %lld, %d, %d, %s, %lld, %lld, %lld, %f, %f, %f, %f, %d, %d, %d",
+        ALG, Q, T1, T2, burst_dist, burstlen, min_burst, max_burst, p0, p1, p2, pg, MAXP, ALLP, OUTMODE
+    );
+
+
+
+    return 0;
+}
